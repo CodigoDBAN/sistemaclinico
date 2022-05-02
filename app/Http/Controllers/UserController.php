a5941ded2dbbebdf413edb14b99ec78e41bdd9cf
david <codigo123xt@gmail.com>
2022-05-01 23:36:54 -0400

    si2

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $users=User::all();
+        return view('user.index',compact('users'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('user.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        date_default_timezone_set("America/La_Paz");
+       //return $request;
+       $users=User::create([
+        'name' => $request['name'],
+        'email' => $request['email'],
+        'password' => password_hash($request['password'],PASSWORD_DEFAULT),
+       ]);
+       return redirect()->route('users.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(User $user)
+    {
+        return view('user.edit',compact('user'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, User $user)
+    {
+    //actualiza nombre
+    if($user->name <> $request->name){
+        $user->name = $request->name;
+    }
+    //actualiza contraseña
+    if($request->password <> ''){
+        $user->password = bcrypt($request->password);
+    }
+
+    if($user->email <> $request->email)
+        $user->email = $request->email;
+             
+    $user->save(); //guardar cambios de usuario 
+    return redirect()->route('users.edit', $user)->with('info', 'se actualizo el usuario correctamente');;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(User $user)
+    {
+        $user->delete();
+        return redirect()->route('users.index');
+    }
+}
